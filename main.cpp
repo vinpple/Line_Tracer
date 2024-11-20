95b3b9578dd33affa273b55541a5176d9ef74dc3
vinpple <rui0607@naver.com>
2024-11-20 15:49:24 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,57 +2,57 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
+int main()
+{
+    string src = "/home/jetson/workspace/LineTracer/simulation/5_lt_cw_100rpm_out.mp4";
 
-int main() {
-    string videoPath = "/home/jetson/workspace/test/simulation/5_lt_cw_100rpm_out.mp4"; 
+    VideoCapture source(src, CAP_FFMPEG);
+    if (!source.isOpened()){ cout << "Camera error" << endl; return -1; }
+    string dst1 = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! h264parse ! rtph264pay pt=96 ! udpsink host=203.234.58.154 port=9011 sync=false";
 
-    VideoCapture source(videoPath, CAP_FFMPEG);
-    if (!source.isOpened()) {
-        cout << "Video file could not be opened!" << endl;
-        return -1;
-    }
+    VideoWriter writer1(dst1, 0, (double)30, Size(640, 360), true);
+    if (!writer1.isOpened()) { cerr << "Writer open failed!" << endl; return -1;}
 
-    string dst1 = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! h264parse ! rtph264pay pt=96 ! udpsink host=203.234.58.154 port=8001 sync=false";
-    VideoWriter writer1(dst1, VideoWriter::fourcc('H', '2', '6', '4'), source.get(CAP_PROP_FPS), Size((int)source.get(CAP_PROP_FRAME_WIDTH), (int)source.get(CAP_PROP_FRAME_HEIGHT)), true);
-    if (!writer1.isOpened()) {
-        cerr << "Writer1 open failed!" << endl;
-        return -1;
-    }
-
-    string dst2 = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! h264parse ! rtph264pay pt=96 ! udpsink host=203.234.58.154 port=8002 sync=false";
-    VideoWriter writer2(dst2, VideoWriter::fourcc('H', '2', '6', '4'), source.get(CAP_PROP_FPS), Size((int)source.get(CAP_PROP_FRAME_WIDTH), (int)source.get(CAP_PROP_FRAME_HEIGHT)), false);
-    if (!writer2.isOpened()) {
-        cerr << "Writer2 open failed!" << endl;
-        return -1;
-    }
-
+    string dst2 = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! h264parse ! rtph264pay pt=96 ! udpsink host=203.234.58.154 port=9012 sync=false";
+    
+    VideoWriter writer2(dst2, 0, (double)30, Size(640, 360), false);
+    if (!writer2.isOpened()) { cerr << "Writer open failed!" << endl; return -1;}
+    
     string dst3 = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! \
         nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! \
         h264parse ! rtph264pay pt=96 ! \
-        udpsink host=203.234.58.154 port=8003 sync=false";
-    VideoWriter writer3(dst3, VideoWriter::fourcc('H', '2', '6', '4'), source.get(CAP_PROP_FPS), Size((int)source.get(CAP_PROP_FRAME_WIDTH), (int)source.get(CAP_PROP_FRAME_HEIGHT)), false);
-    if (!writer3.isOpened()) {
-        cerr << "Writer3 open failed!" << endl;
-        return -1;
-    }
+        udpsink host=203.234.58.154 port=9013 sync=false";
 
-    Mat frame;
+    VideoWriter writer3(dst3, 0, (double)30, Size(640, 90), false);
+    if (!writer3.isOpened()) { cerr << "Writer open failed!" << endl; return -1;}
+    
+
+    cv::TickMeter tm;
+    Mat frame, gray, thresh;
+    
+    int x = 0, y = 270, width = 640, height = 90; // y = 270부터 시작 (하단 90픽셀)
+    Rect roi(x, y, width, height);
+
     while (true) {
-        source >> frame; 
-        if (frame.empty()) {
-            cout << "End of video." << endl;
-            break;
+        tm.start();
+        source >> frame;
+        if (frame.empty()) { 
+            cerr << "frame empty!" << endl; 
+            break; 
         }
+        cvtColor(frame, gray, COLOR_BGR2GRAY); 
+        threshold(gray, thresh, 0, 255, THRESH_OTSU | THRESH_BINARY); 
+        Mat croppedThresh = thresh(roi); 
 
-        writer1 << frame; 
-        writer2 << frame; 
-        writer3 << frame; 
+        writer1 << frame;
+        writer2 << gray;
 
-        imshow("Video", frame); 
-        if (waitKey(30) == 27) { 
-            break;
-        }
+        writer3 << croppedThresh;
+
+        tm.stop();
+        cout << "Time : " << tm.getTimeMilli() << "ms" << endl;
+
+        waitKey(30);
     }
-
     return 0;
 }
